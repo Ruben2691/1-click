1afa299222e76939d2d4f70625850a53aa455871
Pace Ellsworth <pace@flyingovertime.org>
2025-03-07 15:56:24 -0600

    fix user data on layout

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -19,7 +19,7 @@ export default function DefaultLayout({
   useEffect(() => {
     const fectchUser = async () => {
       const supabase = await createClient();
-      const { data: user, error } = await supabase.auth.getUser();
+      const { data: { user }, error } = await supabase.auth.getUser();
       if (error) {
         console.error(error)
         return
