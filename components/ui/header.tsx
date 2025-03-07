be1f0caff48884bf7f25a8b96d36186d44191fc0
Pace Ellsworth <pace@flyingovertime.org>
2025-03-07 16:20:15 -0600

    logo link and glow

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -5,25 +5,25 @@ import { createClient } from '@/utils/supabase/client'
 import { redirect } from 'next/navigation';
 import { User } from '@supabase/supabase-js';
 
-export default function Header({userData}: {userData: User | null}) {
-const signOut = async () => {
-  const supabase = await createClient();
-  const { error } = await supabase.auth.signOut();
-  if (error) {
-    console.error(error)
-    return
+export default function Header({ userData }: { userData: User | null }) {
+  const signOut = async () => {
+    const supabase = await createClient();
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error(error)
+      return
+    }
+    return redirect("/signout");
   }
- return redirect("/signout");
-}
   return (
-    <header className="absolute w-full z-30">
+    <header className="absolute w-full z-30 pt-2">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
 
           {/* Site branding */}
-          <div className="flex-1">
+          <Link href="/" className="relative flex items-center justify-center w-16 h-16 border border-transparent rounded-2xl shadow-2xl [background:linear-gradient(var(--color-slate-900),var(--color-slate-900))_padding-box,conic-gradient(var(--color-slate-400),var(--color-slate-700)_25%,var(--color-slate-700)_75%,var(--color-slate-400)_100%)_border-box] before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-2xl max-w-none shadow-white/30 shadow-md hover:shadow-white/50 hover:shadow-lg transition-shadow duration-300">
             <Logo />
-          </div>
+          </Link>
 
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
@@ -57,15 +57,15 @@ const signOut = async () => {
               </li>
               <li className="ml-6">
                 <form action={signOut}>
-                <button className="btn-sm text-slate-300 hover:text-white transition duration-150 ease-in-out w-full group [background:linear-gradient(var(--color-slate-900),var(--color-slate-900))_padding-box,conic-gradient(var(--color-slate-400),var(--color-slate-700)_25%,var(--color-slate-700)_75%,var(--color-slate-400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none">
-                  <span className="relative inline-flex items-center">
-                    Sign out <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
-                  </span>
+                  <button className="btn-sm text-slate-300 hover:text-white transition duration-150 ease-in-out w-full group [background:linear-gradient(var(--color-slate-900),var(--color-slate-900))_padding-box,conic-gradient(var(--color-slate-400),var(--color-slate-700)_25%,var(--color-slate-700)_75%,var(--color-slate-400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none">
+                    <span className="relative inline-flex items-center">
+                      Sign out <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+                    </span>
                   </button>
                 </form>
               </li>
             </ul>
-          ):( <ul className="flex-1 flex justify-end items-center">
+          ) : (<ul className="flex-1 flex justify-end items-center">
             <li>
               <Link className="font-medium text-sm text-slate-300 hover:text-white whitespace-nowrap transition duration-150 ease-in-out" href="/signin">Sign in</Link>
             </li>
