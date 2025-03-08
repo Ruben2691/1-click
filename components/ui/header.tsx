f62eaa01374649e837b8894bacd1434d919b4244
Ruben Cuello <kuelmusic@gmail.com>
2025-03-07 20:16:43 -0500

    logo adjustments

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -19,15 +19,16 @@ export default function Header({ userData }: { userData: User | null }) {
     <header className="absolute w-full z-30 pt-2">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
-
           {/* Site branding */}
-          <Link href="/" className="relative flex items-center justify-center w-16 h-16 border border-transparent rounded-2xl shadow-2xl [background:linear-gradient(var(--color-slate-900),var(--color-slate-900))_padding-box,conic-gradient(var(--color-slate-400),var(--color-slate-700)_25%,var(--color-slate-700)_75%,var(--color-slate-400)_100%)_border-box] before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-2xl max-w-none shadow-white/30 shadow-md hover:shadow-white/50 hover:shadow-lg transition-shadow duration-300">
+          <Link
+            href="/"
+            className="relative flex items-center justify-center w-14 h-14 border border-transparent rounded-full shadow-2xl [background:linear-gradient(var(--color-slate-900),var(--color-slate-900))_padding-box,conic-gradient(var(--color-slate-400),var(--color-slate-700)_25%,var(--color-slate-700)_75%,var(--color-slate-400)_100%)_border-box] before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-2xl max-w-none shadow-white/30 shadow-md hover:shadow-white/50 hover:shadow-lg transition-shadow duration-300"
+          >
             <Logo />
           </Link>
 
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
-
             {/* Desktop menu links */}
             <ul className="flex grow justify-center flex-wrap items-center">
               {/* <li>
@@ -46,43 +47,61 @@ export default function Header({ userData }: { userData: User | null }) {
                 <Link className="font-medium text-sm text-slate-300 hover:text-white mx-4 lg:mx-5 transition duration-150 ease-in-out" href="/changelog">Changelog</Link>
               </li> */}
             </ul>
-
           </nav>
 
           {/* Desktop sign in links */}
           {userData ? (
             <ul className="flex-1 flex justify-end items-center">
               <li>
-                <Link className="font-medium text-sm text-slate-300 hover:text-white whitespace-nowrap transition duration-150 ease-in-out" href="/account">Account</Link>
+                <Link
+                  className="font-medium text-sm text-slate-300 hover:text-white whitespace-nowrap transition duration-150 ease-in-out"
+                  href="/account"
+                >
+                  Account
+                </Link>
               </li>
               <li className="ml-6">
                 <form action={signOut}>
                   <button className="btn-sm text-slate-300 hover:text-white transition duration-150 ease-in-out w-full group [background:linear-gradient(var(--color-slate-900),var(--color-slate-900))_padding-box,conic-gradient(var(--color-slate-400),var(--color-slate-700)_25%,var(--color-slate-700)_75%,var(--color-slate-400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none">
                     <span className="relative inline-flex items-center">
-                      Sign out <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+                      Sign out{" "}
+                      <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">
+                        -&gt;
+                      </span>
                     </span>
                   </button>
                 </form>
               </li>
             </ul>
-          ) : (<ul className="flex-1 flex justify-end items-center">
-            <li>
-              <Link className="font-medium text-sm text-slate-300 hover:text-white whitespace-nowrap transition duration-150 ease-in-out" href="/signin">Sign in</Link>
-            </li>
-            <li className="ml-6">
-              <Link className="btn-sm text-slate-300 hover:text-white transition duration-150 ease-in-out w-full group [background:linear-gradient(var(--color-slate-900),var(--color-slate-900))_padding-box,conic-gradient(var(--color-slate-400),var(--color-slate-700)_25%,var(--color-slate-700)_75%,var(--color-slate-400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none" href="/signup">
-                <span className="relative inline-flex items-center">
-                  Sign up <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
-                </span>
-              </Link>
-            </li>
-          </ul>)
-          }
+          ) : (
+            <ul className="flex-1 flex justify-end items-center">
+              <li>
+                <Link
+                  className="font-medium text-sm text-slate-300 hover:text-white whitespace-nowrap transition duration-150 ease-in-out"
+                  href="/signin"
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li className="ml-6">
+                <Link
+                  className="btn-sm text-slate-300 hover:text-white transition duration-150 ease-in-out w-full group [background:linear-gradient(var(--color-slate-900),var(--color-slate-900))_padding-box,conic-gradient(var(--color-slate-400),var(--color-slate-700)_25%,var(--color-slate-700)_75%,var(--color-slate-400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none"
+                  href="/signup"
+                >
+                  <span className="relative inline-flex items-center">
+                    Sign up{" "}
+                    <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">
+                      -&gt;
+                    </span>
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          )}
 
           <MobileMenu />
-
         </div>
       </div>
     </header>
-  )
+  );
 }
